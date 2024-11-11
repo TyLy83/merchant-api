7b9fefd8962e0c19ccda6a49dbac9b320d6588e9
Ty Ly <ty.ly81@gmial.com>
2024-11-11 18:15:07 +1300

    fixed db.ts

--- api/db.ts
+++ api/db.ts
@@ -1,19 +1,20 @@
 import { Pool } from "pg";
 import env from "./env";
 
-const pool = new Pool({
-	host: env.POSTGRES_HOST,
-	user: env.POSTGRES_USER,
-	password: env.POSTGRES_PASSWORD,
-	database: env.POSTGRES_DATABASE,
-	port: env.POSTGRES_PORT,
-	idleTimeoutMillis: 30000,
-});
-
-// // :TO DEPLOY TO VERCEL
-// // : uncomment this config for production
 // const pool = new Pool({
-// 	connectionString: env.POSTGRES_URL,
+// 	host: env.POSTGRES_HOST,
+// 	user: env.POSTGRES_USER,
+// 	password: env.POSTGRES_PASSWORD,
+// 	database: env.POSTGRES_DATABASE,
+// 	port: env.POSTGRES_PORT,
+// 	idleTimeoutMillis: 30000,
 // });
+
+// :TO DEPLOY TO VERCEL
+// : uncomment this config for production
+
+const pool = new Pool({
+	connectionString: env.POSTGRES_URL,
+});
 
 export default pool;
